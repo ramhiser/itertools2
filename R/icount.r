aeef897999e02945fdf9c43f60cc86654b6c1df4
John Ramey <johnramey@gmail.com>
2015-07-31 21:30:29 -0500

    Updated docs about icount's stop argument. #39

--- R/icount.r
+++ R/icount.r
@@ -1,7 +1,9 @@
-#' Iterator of neverending numeric sequence with initial value and step size
+#' Iterator of numeric sequence with initial value and step size
 #'
-#' Constructs an iterator that generates a neverending sequence of evenly spaced
-#' values starting with \code{icount}. The step size is given by \code{step}.
+#' Constructs an iterator that generates a sequence of evenly spaced values
+#' starting with \code{icount}. The step size is given by \code{step}. By
+#' default, the sequence is neverending unless the optional \code{stop} is
+#' provided.
 #'
 #' NOTE: Use a negative \code{step} size to generate decreasing sequences.
 #'
@@ -11,6 +13,7 @@
 #' @export
 #' @param start sequence's initial value
 #' @param step sequence's step size
+#' @param stop optional stopping point to sequence
 #' @return sequence's iterator
 #'
 #' @examples
@@ -23,18 +26,28 @@
 #' iterators::nextElem(it2)
 #' iterators::nextElem(it2)
 #' iterators::nextElem(it2)
+#'
+#' it3 <- icount(start=1, stop=3)
+#' iterators::nextElem(it3)
+#' iterators::nextElem(it3)
+#' iterators::nextElem(it3)
+#'
 icount <- function(start=0, step=1, stop=NULL) {
   start <- as.numeric(start)
   step <- as.numeric(step)
-  if (!is.null(stop)) {
-    stop <- as.numeric(stop)
-  }
 
   if (length(start) != 1) {
     stop("'start' must be a numeric value of length 1")
   }
   if (length(step) != 1) {
     stop("'step' must be a numeric value of length 1")
+  }
+
+  if (!is.null(stop)) {
+    stop <- as.numeric(stop)
+    if (length(stop) != 1) {
+      stop("'stop' must be a numeric value of length 1")
+    }
   }
 
   current_val <- start - step
